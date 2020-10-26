7f40eacc550fb28349169b3404e271ca5ff0d00b
kishore007k <kishorekumarstar07@gmail.com>
2020-10-26 14:00:48 +0530

    Added a new Backend for trail and finished the Experience
    Signed-off-by: kishore007k <kishorekumarstar07@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,11 +24,11 @@ module.exports = {
       options: {
         apiURL: `http://localhost:1337`,
         queryLimit: 1000, // Default to 100
-        //   contentTypes : `jobs`, `projects`, `blogs`,
-        //   singleType : `about`
+        contentTypes: [`jobs`, `projects`, `blogs`],
+          singleType : `about`
         //  ONLY ADD TO ARRAY IF YOU HAVE DATA IN STRAPI !!!!
-        contentTypes: [`jobs`, `skills`],
-        singleTypes: [],
+        // contentTypes: [`jobs`, `skills`],
+        // singleTypes: [],
       },
     },
     {
